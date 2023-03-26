f83e6be970cbbecc8db3a1b5c8e36bca51ea54f9
Laise France <laise94@hotmail.com>
2023-03-26 19:31:55 -0300

    add validations

--- api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,7 +1,26 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 //DTO
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, Consultorio consultorio, Sala sala, DadosEndereco endereco) {
+public record DadosCadastroMedico(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        Consultorio consultorio,
+        Sala sala,
+        @NotNull @Valid DadosEndereco endereco) {
 
 }
